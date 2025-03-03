a0ad7ecb537771dcbb9576caa9d2adbd17ce2a6e
Rares-AndreiVasiu <rares_vasiu03@yahoo.ro>
2025-03-03 20:22:25 +0200

    added enums for lexical analysis

---
+++ include/atom_token.h
@@ -0,0 +1,68 @@
+#ifndef enums
+
+enum identifier
+{
+    ID
+};
+
+enum keyword
+{
+    BREAK,
+    CHAR,
+    DOUBLE,
+    ELSE,
+    FOR,
+    IF,
+    INT,
+    RETURN,
+    STRUCT,
+    VOID,
+    WHILE
+};
+
+enum constans
+{
+    CT_INT,
+    CT_REAL,
+    CT_CHAR,
+    CT_STRING
+};
+
+enum delimiter
+{
+    COMMA,
+    SEMICOLON,
+    LPAR,
+    RPAR,
+    LBRACKET,
+    RBRACKET,
+    LACC,
+    RACC
+};
+
+enum opreators
+{
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    DOT,
+    AND,
+    OR,
+    NOT,
+    ASSIGN,
+    EQUAL,
+    NOTEQ,
+    LESS,
+    LESSEQ,
+    GREATER,
+    GREATEREQ 
+};
+
+enum sugar
+{
+    SPACE,
+    LINECOMMENT,
+    COMMENT
+};
+#endif // ! enums
---
+++ src/lexical_analyzer.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/atom_token.h"
+
+int main()
+{
+    return 0;
+}
