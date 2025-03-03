327c93daccd5917abd532f29cb5a911e4e7c8904
Rares-AndreiVasiu <rares_vasiu03@yahoo.ro>
2025-03-03 20:26:37 +0200

    added token struct

--- include/atom_token.h
+++ include/atom_token.h
@@ -65,4 +65,26 @@ enum sugar
     LINECOMMENT,
     COMMENT
 };
+
+typedef struct _token
+{
+    int code,
+
+    union
+    {
+        //for id, CT_STRING
+        char *text;
+            
+        // for CT_INT, CT_CHAR
+        long int i;
+        
+        //for ct_real
+        double r;
+    };
+
+    int line;
+
+    struct _token *next;
+
+}token;
 #endif // ! enums
